c6b71613de2c88e524c980800c4a02fd34c1668b
Carlos <carlosgalindo@gmail.com>
2014-10-02 15:46:52 -0500

    2014-10-02-b

--- mysite/lab/models.py
+++ mysite/lab/models.py
@@ -317,7 +317,7 @@ class ForceNode(AbstractTree):
     locs = _many('Loc', through='ForceVisit')
 
     def __unicode__(self):
-        return _str(self, '%s %s: %s', (self.h_level(), self.name, self.user))
+        return _str(self, '%s %s: %s', (self.h_level(), self.name, self.user or '-'))
 
     def itemcats_(self):
         return multiple_(self, 'itemcats')
